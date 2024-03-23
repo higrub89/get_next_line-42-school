ec4443e52d305dd22076cb74c4936aa579f18f5a
Ruben Dario Higuita Rubio <rhiguita@c2r1s5.42madrid.com>
2024-03-23 21:31:53 +0100

    Define headers *.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,17 @@
 /*   By: rhiguita <rhiguita@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 20:37:52 by rhiguita          #+#    #+#             */
-/*   Updated: 2024/03/23 20:37:59 by rhiguita         ###   ########.fr       */
+/*   Updated: 2024/03/23 21:25:43 by rhiguita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// archivo abecera.
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+char	*get_next_line(int fd);
+
+#endif
